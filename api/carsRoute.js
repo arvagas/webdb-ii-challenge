b7bfe6c5466dd6828052f71edc3226afdee2feaf
Arvin Agas <arvagas@gmail.com>
2019-09-10 16:35:16 -0400

    POST request functional

--- api/carsRoute.js
+++ api/carsRoute.js
@@ -4,10 +4,26 @@ const db = require('../data/db-config')
 
 const router = express.Router()
 
+// @@@@@@@@@@ GET request @@@@@@@@@@
 router.get('/', (req, res) => {
     db('cars')
     .then(cars => res.json(cars))
     .catch(err => res.status(500).json({ message: 'Failed to retrieve cars' }))
+})
+
+// @@@@@@@@@@ POST request @@@@@@@@@@
+router.post('/', (req, res) => {
+    const newCar = req.body
+
+    db('cars')
+    .insert(newCar, 'id')
+    .then(([id]) => {
+        db('cars')
+        .where({ id })
+        .then(car => res.json(car))
+        .catch(err => res.status(500).json({ message: 'Failed to retrieve car' }))
+    })
+    .catch(err => res.status(500).json({ message: 'Failed to add new car' }))
 })
 
 module.exports = router
